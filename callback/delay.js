c13dbcb5718885cf21d28e21f55d20aaa4c8c134
Yaksh2004 <gargyaksh8@gmail.com>
2024-08-17 21:21:33 +0530

    callback easy

---
+++ callback/delay.js
@@ -0,0 +1,14 @@
+// Write a function delayedMessage(message, delay, callback) that takes a message,
+//  a delay (in milliseconds), and a callback function. The function should log the
+//   message after the specified delay, then call the callback.
+
+function delayedMessage(message, delay, callback){
+    setTimeout(() => {
+        console.log(message);
+        callback;
+    }, delay);
+};
+
+delayedMessage("Hello World!", 3000, function(){
+    console.log("This mesasge was delayed by 3 seconds")
+});
